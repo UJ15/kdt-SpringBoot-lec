e1cc041643f2c7653939a5a0dd381d12acede9af
UJ15 <dbwogml15@naver.com>
2022-04-07 17:10:39 +0900

    yml 강의 후

--- kdt-spring-order/src/main/java/org/prgrms/kdt/AppConfiguration.java
+++ kdt-spring-order/src/main/java/org/prgrms/kdt/AppConfiguration.java
@@ -1,16 +1,15 @@
 package org.prgrms.kdt;
 
-import org.springframework.context.annotation.Bean;
+import org.prgrms.kdt.configuration.YamlPropertiesFactory;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-import java.util.Optional;
-import java.util.UUID;
-
 @Configuration
 @ComponentScan (basePackages = {"org.prgrms.kdt.order","org.prgrms.kdt.voucher"})//패키지 범위를 정해줬다.
-@PropertySource("application.properties")
+@PropertySource(value = "application.yaml", factory = YamlPropertiesFactory.class) //야믈 설정파일로 잡으려면
+@EnableConfigurationProperties //ComfigurationProperties를 쓸 수 있게
 public class AppConfiguration {
 
 }
--- kdt-spring-order/src/main/java/org/prgrms/kdt/OrderTest.java
+++ kdt-spring-order/src/main/java/org/prgrms/kdt/OrderTest.java
@@ -3,6 +3,7 @@ package org.prgrms.kdt;
 import org.prgrms.kdt.AppConfiguration;
 import org.prgrms.kdt.order.Order;
 import org.prgrms.kdt.order.OrderItem;
+import org.prgrms.kdt.order.OrderProperties;
 import org.prgrms.kdt.order.OrderService;
 import org.prgrms.kdt.voucher.FixedAmountVoucher;
 import org.prgrms.kdt.voucher.Voucher;
@@ -11,19 +12,38 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.BeanFactoryAnnotationUtils;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.util.Assert;
 
 import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
+
+
 
 public class OrderTest {
     public static void main(String[] args) {
         var applicationContext = new AnnotationConfigApplicationContext(AppConfiguration.class);
 
-        var customerId = UUID.randomUUID();
+//        ConfigurableEnvironment environment = applicationContext.getEnvironment();
+//        String version = environment.getProperty("kdt.version");
+//        Integer minimumOrderAmount = environment.getProperty("kdt.minimum-order-amount", Integer.class);
+//        List<String> supportVendors = environment.getProperty("kdt.support-vendors", List.class);
+//        var description = environment.getProperty("kdt.description", List.class);
+//        System.out.println(MessageFormat.format("version -> {0}", version));
+//        System.out.println(MessageFormat.format("minimumOrderAmount -> {0}", minimumOrderAmount));
+//        System.out.println(MessageFormat.format("supportVendors -> {0}", supportVendors));
+//        System.out.println(MessageFormat.format("description -> {0}", description));
+        OrderProperties orderProperties = applicationContext.getBean(OrderProperties.class);
+        System.out.println(MessageFormat.format("version {0}", orderProperties.getVersion()));
+        System.out.println(MessageFormat.format("minimumOrderAmount {0}", orderProperties.getMinimumOrderAmount()));
+        System.out.println(MessageFormat.format("supportVendors {0}", orderProperties.getSupportVendors()));
+        System.out.println(MessageFormat.format("description {0}", orderProperties.getDescription()));
 
+
+        var customerId = UUID.randomUUID();
         VoucherRepository voucherRepository = BeanFactoryAnnotationUtils.qualifiedBeanOfType(applicationContext.getBeanFactory(), VoucherRepository.class, "memory");
         Voucher voucher = voucherRepository.insert(new FixedAmountVoucher(UUID.randomUUID(), 10L));
 
